bc6bcfc14f84cb5221c705151fa889ba7d2935eb
Kym Michelle McCormick <a1191925@adelaide.edu.au>
2019-02-22 10:32:00 +1030

    This is the next step!

--- test script.R
+++ test script.R
@@ -1,1 +1,12 @@
-#this is a test script
+
+mu <- 1
+sigma <- 1
+TPFoilValues <- matrix(rnorm(5000), nrow = 1000)
+TPTargetValues <- matrix(rnorm(1000,mu,sigma), nrow = 1000)
+TPValues <- cbind(TPFoilValues, TPTargetValues)
+TAValues <- matrix(rnorm(6000), nrow = 1000)
+head(TPValues, n=10)
+head(TAValues, n=10)
+hist(TPFoilValues)
+hist(TPTargetValues)
+hist(TAValues)
